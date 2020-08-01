f6e327b3659fb53314e16c0fe62d258afbe76001
andreaouage <andreaouga@gmail.com>
2020-08-01 13:04:02 +0200

    second commit

--- source/setup.py
+++ source/setup.py
@@ -3,6 +3,6 @@ from . import constants as C
 from . import tools
 
 pygame.init()
-pygame.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))
 
 GRAPHICS = tools.load_graphics('resources/graphics')
--- source/states/main_menu.py
+++ source/states/main_menu.py
@@ -1,6 +1,7 @@
 import pygame
 from .. import setup
 from .. import tools
+from .. import constants as C
 
 class MainMenu:
     def __init__(self):
@@ -8,15 +9,25 @@ class MainMenu:
         self.setup_player()
         self.setup_cursor()
 
-    def setup_background(self, parameter_list):
+    def setup_background(self):
+        self.background = setup.GRAPHICS['level_1']
+        self.background_rect = self.background.get_rect()
+        self.background = pygame.transform.scale(self.background, 
+        (int(self.background_rect.width * C.BG_MULTI), 
+        int(self.background_rect.height * C.BG_MULTI)))
+
+        self.viewpoint = setup.SCREEN.get_rect()
+        self.caption = tools.get_image(
+            setup.GRAPHICS['title_screen'], 
+            1, 60, 176, 88, (255, 0 ,220), C.BG_MULTI)
+        
+
+    def setup_player(self):
         pass
 
-    def setup_player(self, parameter_list):
-        pass
-
-    def setup_cursor(self, parameter_list):
+    def setup_cursor(self):
         pass
 
     def update(self, surface):
-        import random
-        surface.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        surface.blit(self.background, self.viewpoint)
+        surface.blit(self.caption, (170, 100))
