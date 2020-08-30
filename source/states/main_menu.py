02fab5762d734c463ad71b55d70f20c6ef38d7b6
andreaouage <andreaouga@gmail.com>
2020-08-30 20:00:50 +0200

    add information of main menu

--- source/states/main_menu.py
+++ source/states/main_menu.py
@@ -2,12 +2,14 @@ import pygame
 from .. import setup
 from .. import tools
 from .. import constants as C
+from ..components import info
 
 class MainMenu:
     def __init__(self):
         self.setup_background()
         self.setup_player()
         self.setup_cursor()
+        self.info = info.Info('main_menu')
 
     def setup_background(self):
         self.background = setup.GRAPHICS['level_1']
@@ -33,7 +35,11 @@ class MainMenu:
             3, 155, 8, 8, (255, 0, 220), C.BG_MULTI)
 
     def update(self, surface):
+
         surface.blit(self.background, self.viewpoint)
         surface.blit(self.caption, (170, 100))
         surface.blit(self.cursor_image, (220, 360))
         surface.blit(self.player_image, (200, 495))
+
+        self.info.update()
+        self.info.draw(surface)
